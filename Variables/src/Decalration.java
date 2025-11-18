5e008d6b14b4179f27effc7c9dc0fa3603b88b25
deepakmanju <deepakmanju2002@gmail.com>
2025-11-18 22:35:25 +0530

    task 18/11/2025

---
+++ Variables/src/Decalration.java
@@ -0,0 +1,14 @@
+public class Decalration {
+    public static void main(String[] args) {
+        int age = 23;
+        double height = 5.7 ;
+        char grade = 'A';
+        boolean isStudent = true;
+        String studentName = "Deepak";
+        System.out.println("Age :"+age);
+        System.out.println("Height :"+height);
+        System.out.println("Grade :"+grade);
+        System.out.println("Student :"+isStudent);
+        System.out.println("Student Name :"+studentName);
+    }
+}
---
+++ Variables/src/Reassignment.java
@@ -0,0 +1,8 @@
+public class Reassignment {
+    public static void main(String[] args) {
+        int Number = 50 ; // Before reassign
+        System.out.println("Before Assigment :"+Number);
+         Number = 70 ;  //// Just reassign, no need to write 'int' again
+        System.out.println("After Assigment :"+Number);
+    }
+}
